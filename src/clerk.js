9ac8fdbf348d256ebaf95c444f3218c66118a1cc
masan4444 <masanori0213@gmail.com>
2020-09-17 20:50:25 +0900

    [fix] hooksの導入

--- src/clerk.js
+++ src/clerk.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom";
 import Header from "./react-components/header";
 import { WrappedIntlProvider } from "./react-components/wrapped-intl-provider";
@@ -10,10 +10,10 @@ import { AuthContextProvider } from "./react-components/auth/AuthContext";
 // import { SignInPage } from "./react-components/auth/SignInPage";
 import "./assets/stylesheets/globals.scss";
 // import { Link } from "react-router-dom";
-import { spacing } from "@material-ui/system";
 
-import { Box, Grid, TextField } from "@material-ui/core";
+import { Box, Button, Grid, TextField } from "@material-ui/core";
 import Autocomplete from "@material-ui/lab/Autocomplete";
+import SaveIcon from "@material-ui/icons/Save";
 
 // import { AuthContextProvider } from "./scene-entry-manager";
 
@@ -29,39 +29,76 @@ function click(url) {
   // spawnMediaInfrontOfPlayer();
 }
 
-const todofuken = [
+const prefectureList = [
   { value: "hokkaido", label: "北海道" },
   { value: "tokyo", label: "東京都" },
   { value: "osaka", label: "大阪府" }
 ];
 
 function Root() {
+  const [prefecture, setPrefecture] = useState("東京都");
+  const [town, setTown] = useState("京都市中京区 二条下ル土橋町10番地");
+  const [shopName, setShopName] = useState("京みやげ売店");
+
   return (
     <WrappedIntlProvider locale={getLocale()} messages={getMessages()}>
       <AuthContextProvider store={store}>
         <Header />
-        <Box marginTop={12} marginBottom={3} marginX={3}>
-          <Grid container spacing={0}>
-            <Grid item xs>
-              <Autocomplete
-                marginY={10}
-                id="combo-box-demo"
-                options={todofuken}
-                getOptionLabel={option => option.label}
-                // style={{ width: 400 }}
-                renderInput={params => <TextField {...params} label="都道府県" variant="outlined" />}
-              />
-              <TextField
-                marginY={10}
-                required
-                id="standard-basic"
-                label="市町村区"
-                defaultValue="京都市中京区 二条下ル土橋町10番地"
-              />
-              <TextField marginY={10} required id="standard-required" label="店名" defaultValue="京みやげ売店" />
+        <Box marginTop={8} marginX={3}>
+          {/* <Grid container spacing={0}> */}
+          {/* <Grid item xs> */}
+          <Box my={4}>
+            <Autocomplete
+              id="combo-box-demo"
+              options={prefectureList}
+              getOptionLabel={option => option.label}
+              // style={{ width: 400 }}
+              // onChange={e => {
+              //   console.log(e.target.value);
+              //   setPrefecture(e.target.label);
+              // }}
+              renderInput={params => (
+                <TextField
+                  onChange={e => {
+                    console.log(e.target);
+                    setPrefecture(e.target.label);
+                  }}
+                  {...params}
+                  label="都道府県"
+                  variant="outlined"
+                />
+              )}
+            />
+          </Box>
+          <Box my={4}>
+            <TextField
+              required
+              id="standard-basic"
+              label="市町村区"
+              defaultValue={town}
+              fullWidth
+              onChange={() => console.log(prefecture)}
+            />
+          </Box>
+          <Box my={4}>
+            <TextField
+              required
+              id="standard-required"
+              label="店名"
+              defaultValue={shopName}
+              fullWidth
+              onChange={() => console.log(prefecture)}
+            />
+          </Box>
+          <Box m="auto">
+            <Grid container justify="center">
+              <Button variant="contained" color="primary" size="large" startIcon={<SaveIcon />}>
+                Save
+              </Button>
             </Grid>
-          </Grid>
-          aaa
+          </Box>
+          {/* </Grid> */}
+          {/* </Grid> */}
         </Box>
       </AuthContextProvider>{" "}
     </WrappedIntlProvider>
